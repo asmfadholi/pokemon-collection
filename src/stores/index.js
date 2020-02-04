1cd1873feef56b2cddf13ba8fd1b9db50ea8dd17
asmfadholi <asmfadholi@gmail.com>
2020-02-05 06:31:57 +0700

    setup secure state management data and their persistant

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import PokemonStore from './modules/PokemonStore'
+import SecureLS from 'secure-ls'
+
+let ls = new SecureLS({ isCompression: false })
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: key => ls.remove(key)
+      }
+    })
+  ],
   modules: {
     PokemonStore
   }
