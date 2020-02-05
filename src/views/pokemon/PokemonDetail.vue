d835c72d70814422639b3e984fde1ec3279e8e04
asmfadholi <asmfadholi@gmail.com>
2020-02-05 19:26:58 +0700

    change redirect path after save new pokemon

--- src/views/pokemon/PokemonDetail.vue
+++ src/views/pokemon/PokemonDetail.vue
@@ -142,7 +142,7 @@ export default {
     savePokemon () {
       this.$store.commit('PokemonStore/collectData', this.itemData)
       this.$refs.modal.hide()
-      this.$router.replace('/pokemon/collection')
+      this.$router.replace('/pokemon')
     },
 
     hideToRedirect () {
